3e8de5245e0e9f7a75b47068431414cb3be5ff3b
Daethalus <daethalus@gmail.com>
2024-07-25 00:37:21 +0100

    fix optional

--- Engine/Source/Fyrion/Core/Optional.hpp
+++ Engine/Source/Fyrion/Core/Optional.hpp
@@ -7,13 +7,27 @@ namespace Fyrion
     template<typename T>
     class Optional : public std::optional<T>
     {
+    public:
+
+        Optional() : std::optional<T>()
+        {
+        }
+
+        template<typename ...Args>
+        Optional(Args&&... args) : std::optional<T>(Traits::Forward<Args>(args)...)
+        {
+        }
+
+        Optional(const T& value) : std::optional<T>(value)
+        {
+        }
     };
 
 
     template<typename T, typename ...Args>
     Optional<T> MakeOptional(Args&&... args)
     {
-        return static_cast<Optional<T>>(std::make_optional<T>(Traits::Forward<Args>(args)...));
+        return Optional(Traits::Forward<Args>(args)...);
     }
 
 }
